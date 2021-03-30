115985013fdc81f2cfba3ebf4bcb7e99e92f9f35
Brown Mamba <brdyherrera@gmail.com>
2021-03-30 19:05:10 -0400

    added book model and linked

--- Develop/server/models/Author.js
+++ Develop/server/models/Author.js
@@ -1,4 +1,17 @@
 const { Schema } = require('mongoose');
 
+const authorSchema = new Schema(
+    {
+        authorTitle: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
 
 module.exports = authorSchema;
--- Develop/server/models/Book.js
+++ Develop/server/models/Book.js
@@ -1,27 +1,33 @@
 const { Schema } = require('mongoose');
+const authorSchema = require('./Author');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
-const bookSchema = new Schema({
-  author: [authorSchema],
-  description: {
-    type: String,
-    required: true
-  },
-  // saved book id from GoogleBooks
-  bookId: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String,
-  },
-  link: {
-    type: String
-  },
-  title: {
-    type: String,
-    required: true
+const bookSchema = new Schema(
+  {
+    author: [authorSchema],
+    description: {
+      type: String,
+      required: true
+    },
+    // saved book id from GoogleBooks
+    bookId: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+    },
+    link: {
+      type: String
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    author: [authorSchema]
   }
-});
+);
 
-module.exports = bookSchema;
+const Book = model('Book', bookSchema);
+
+module.exports = Book;
