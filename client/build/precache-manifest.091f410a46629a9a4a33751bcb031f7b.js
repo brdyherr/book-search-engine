3ccc1f5613a17e8fca7ec3c63f503416f2d2b558
Brown Mamba <brdyherrera@gmail.com>
2021-04-18 07:14:49 -0400

    Adding the production build files

---
+++ client/build/precache-manifest.091f410a46629a9a4a33751bcb031f7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ccbe6aef440e70b6be37cb88f40c66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8782215d7528aed262cf",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "474252dfef92336208d7",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "8782215d7528aed262cf",
+    "url": "/static/js/2.c5cd8f9f.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/static/js/2.c5cd8f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474252dfef92336208d7",
+    "url": "/static/js/main.2bb4d47d.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
